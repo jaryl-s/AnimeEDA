29f7e3eda4babdaea71ae8052ffe818a92de28d2
jaryl-s <93922854+jaryl-s@users.noreply.github.com>
2022-11-20 02:07:06 +0800

    Update datasetanalysis.py

--- datasetanalysis.py
+++ datasetanalysis.py
@@ -32,10 +32,8 @@ fig1 = plt.figure(figsize=(10, 6))
 # Plot graph
 ax1 = fig1.add_subplot(111)
 sns.kdeplot(anime_df['rating'],
- kernel='gau', # Code for shape of kernel to fit with. Bivariate KDE can onl
-y use Gaussian kernel
- bw=0.4, # bw affects curve smoothness. Small bw is precise but potentially
-messy, large bw is smooth but vague
+ kernel='gau', # Code for shape of kernel to fit with. Bivariate KDE can only use Gaussian kernel
+ bw=0.4, # bw affects curve smoothness. Small bw is precise but potentially messy, large bw is smooth but vague
  shade=True)
 # Define axis titles and title
 ax1.set_xlabel("rating", fontsize = 15)
@@ -44,23 +42,16 @@ ax1.set_title("KDE plot of Anime Rating Data", fontsize = 18)
 plt.show()
 
 # analyse anime by type
-# From anime_df, group the entries by 'type', and sort them in descending order of the
-count for each type
-anime_types = anime_df.groupby('type').size().reset_index(name = 'count').sort_values(
-'count', ascending = False)
-# Create a new dataframe - Adding 'drop = True' within .reset_index() eliminates the 'i
-ndex' column that is not needed here
+# From anime_df, group the entries by 'type', and sort them in descending order of the count for each type
+anime_types = anime_df.groupby('type').size().reset_index(name = 'count').sort_values('count', ascending = False)
+# Create a new dataframe - Adding 'drop = True' within .reset_index() eliminates the 'index' column that is not needed here
 anime_types_df = pd.DataFrame(data = anime_types.reset_index(drop = True))
 # Note that the most numerous anime type will be in the first row, at index 0
 display(anime_types_df)
-total_count = anime_types_df['count'].sum() # This calculates the total number of anim
-e series
-highest_type_count = anime_types_df['count'][0] # This calculates the number of anime
-series of the most numerous type
-highest_type_proportion = highest_type_count / total_count # This calculates the propo
-rtion of the most numerous type
-print('The proportion of ' + str(anime_types_df['type'][0]) + '-type anime is: ' + str(
-highest_type_proportion))
+total_count = anime_types_df['count'].sum() # This calculates the total number of anime series
+highest_type_count = anime_types_df['count'][0] # This calculates the number of anime series of the most numerous type
+highest_type_proportion = highest_type_count / total_count # This calculates the proportion of the most numerous type
+print('The proportion of ' + str(anime_types_df['type'][0]) + '-type anime is: ' + str(highest_type_proportion))
 # Plot data as a horizontal bar chart
 plt.figure(figsize = (12,8))
 plt.barh(anime_types['type'], # y-axis of the bar chart
@@ -79,16 +70,13 @@ plt.ylabel('Type', fontsize =15)
 genres = set()
 # Iterate through each entry, with focus on the genre column
 for entry in anime_df['genre']:
- # Without the condition below, AttributeError will occur: 'float' object has no att
-ribute 'split'
- # If the genre isn't a string, the continue' statement continues to the next iterat
-ion of the loop (skip)
+ # Without the condition below, AttributeError will occur: 'float' object has no attribute 'split'
+ # If the genre isn't a string, the continue' statement continues to the next iteration of the loop (skip)
  if type(entry) is not str:
  continue
  # The split() method splits a string into a list. Specify separator as comma.
  genres.update(entry.split(", "))
-# Note that a set cannot have duplicate elements by definition. Thus there is no need t
-o deliberately prevent double-adding
+# Note that a set cannot have duplicate elements by definition. Thus there is no need to deliberately prevent double-adding
 print(genres)
 print("Total Genres: " + str(len(genres)))
 
@@ -101,8 +89,7 @@ for entry in anime_df['genre']:
  # Same as above, only proceed with 'entry' if it is a string type
  if type(entry) is not str:
  continue
- # 'entry.split(", ")' can look something like this: ['Horror', 'Comedy', 'Action',
-'Drama']
+ # 'entry.split(", ")' can look something like this: ['Horror', 'Comedy', 'Action', 'Drama']
  for genre in entry.split(", "):
  genres_count[genre] += 1
 
@@ -112,17 +99,14 @@ print(genres_count)
 # Iterable: Collection that needs to be sorted
 # Key: Basis for comparison
 # Reverse: Descending (True) or ascending (False)
-# operator.itemgetter(1) constructs a callable that assumes an iterable object (e.g. li
-st, tuple, set) as input,
-# and fetches the 1st element out of it. In this case, the 1st element for each nested
-tuple is the count for the genre
+# operator.itemgetter(1) constructs a callable that assumes an iterable object (e.g. list, tuple, set) as input,
+# and fetches the 1st element out of it. In this case, the 1st element for each nested tuple is the count for the genre
 
 # Initialise two empty lists to store each genre, as well as the count for each genre
 genre_list = []
 genre_count_list = []
 # Recall that genres_count is a list of tuples, with each tuple containing the genre and its count: e.g. ('Comedy', 4645)
-# Iterate through each nested tuple within the list, and append the genre and its count
-to the two lists
+# Iterate through each nested tuple within the list, and append the genre and its count to the two lists
 for each_tuple in genres_count:
  genre_list.append(each_tuple[0])
  genre_count_list.append(each_tuple[1])
